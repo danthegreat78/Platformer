0698d5567a03a1021a7abc8a28718541c7a51613
danesh <danesh2020@icloud.com>
2026-06-04 21:49:12 -0400

    Integrated new Player Class and fixed physics bugs.

--- Player.py
+++ Player.py
@@ -6,15 +6,41 @@ class Player:
         self.image = pygame.image.load("player.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (1000,1000))
 
-        self.pos = pygame.Vector2(x, y)
+        self.hitbox = pygame.Rect(x,y,90,250)
+
         self.velocity_y = 0
 
         self.gravity = 1800
+
         self.on_ground = False
 
-        self.hitbox = pygame.rect(0,0,90,250)
+        self.jumper_buffer = 0
 
-        def update(self, dt, keys, ground_rect):
+        self.draw_offset = pygame.Vector2(-500, -190)
+
+
+
+    def update(self, dt, keys, ground_rect):
+
+        if(self.jumper_buffer > 0):
+            self.jumper_buffer -= dt
+
+
+        self.velocity_y += self.gravity * dt
+        self.hitbox.y += self.velocity_y * dt
+
+
+
+        # Horizontal Movements
+        speed = 1000
+        if keys[pygame.K_a]:
+            self.hitbox.x -= speed * dt
+        if keys[pygame.K_d]:
+            self.hitbox.x += speed * dt
+
+        self.on_ground = False
+
+        if self.hitbox.colliderect(ground_rect):
             left = self.hitbox.right - ground_rect.left
             right = ground_rect.right - self.hitbox.left
             bottom = self.hitbox.bottom - ground_rect.top
@@ -22,20 +48,23 @@ class Player:
 
             if min(left, right, bottom, top) == bottom:
                 self.hitbox.bottom = ground_rect.top
-                self.pos.y = self.hitbox.centery
                 self.velocity_y = 0
-                on_ground = True
+                self.on_ground = True
 
             elif min(left, right, bottom, top) == right:
                 self.hitbox.left = ground_rect.right
-                self.hitbox.x = ground_rect.centerx
-            speed = 1000
 
-            # Horizontal Movements
-            if keys[pygame.K_a]:
-                self.pos.x -= speed * dt
-            if keys[pygame.K_d]:
-                self.pos.x += speed * dt
-
-            if (keys[pygame.K_w] or keys[pygame.K_SPACE]) and self.on_ground:
+        if self.jumper_buffer > 0:
+            if self.on_ground:
                 self.velocity_y = -1000
+                self.on_ground = False
+                self.jump_request = False
+
+
+
+
+
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.hitbox.x + self.draw_offset.x, self.hitbox.y + self.draw_offset.y))
+        pygame.draw.rect(screen, "red", self.hitbox, 2)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from Player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -6,80 +7,41 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player = pygame.image.load("player.png").convert_alpha()
+player = Player(640, 100)
 
-player = pygame.transform.scale(player, (1000, 1000))
 
-player_pos = pygame.Vector2(640, 100)
 
-velocity_y = 0
 
-gravity = 1800
 ground_y = pygame.Rect(0, 680, 1000, 40)
-wall_x = 1240
-on_ground = False
 
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or event.key == pygame.K_SPACE:
+                player.jumper_buffer = 0.1
 
 
 
-    velocity_y += gravity * dt
-    player_pos.y += velocity_y * dt
-
-    on_ground = False
-
-    player_hitbox = pygame.Rect(0,0,90,250) # Hitbox adjustment
-    player_hitbox.center = player_pos
-
-    if player_hitbox.colliderect(ground_y):
-
-        left = player_hitbox.right - ground_y.left
-        right = ground_y.right - player_hitbox.left
-        bottom = player_hitbox.bottom - ground_y.top
-        top = ground_y.bottom - player_hitbox.top
-
-        if min(left, right, bottom, top) == bottom:
-            player_hitbox.bottom = ground_y.top
-            player_pos.y = player_hitbox.centery
-            velocity_y = 0
-            on_ground = True
-        elif min(left, right, bottom, top) == right:
-            player_hitbox.left = ground_y.right
-            player_pos.x = player_hitbox.centerx
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_w] or keys[pygame.K_SPACE]:
-        if on_ground:
-            velocity_y = -1000
 
 
-
-
+    player.update(dt, keys, ground_y)
 
     screen.fill("purple")
 
     pygame.draw.rect(screen, "green", ground_y)
 
-    screen.blit(player, (player_pos.x - player.get_width() / 2 -50,
-                         player_pos.y - player.get_height() / 2 + 185)) #ADJUST THIS
-    pygame.draw.rect(screen, "red", player_hitbox, 2) # Shows player hitbox
+    player.draw(screen)
 
+   # screen.blit(player, (player_pos.x - player.get_width() / 2 -50,
+    #                     player_pos.y - player.get_height() / 2 + 185)) #ADJUST THIS
+    #pygame.draw.rect(screen, "red", player_hitbox, 2) # Shows player hitbox
 
-
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_s]:
-        player_pos.y += 1000 * dt
-
-    if keys[pygame.K_a]:
-        player_pos.x -= 1000 * dt
-
-    if keys[pygame.K_d]:
-        player_pos.x += 1000 * dt
 
     pygame.display.flip()
     pygame.display.set_caption("Platformer")
